67bff52e64889370dda9a65b66afff44fc209b44
cicisajaaa <shahlaqeysha@gmail.com>
2025-06-30 01:17:42 +0800

    Menambahkan code untuk tampilan

--- src/reminderminumair/ReminderMinumair.java
+++ src/reminderminumair/ReminderMinumair.java
@@ -7,5 +7,33 @@ public class ReminderMinumair {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         LocalDate hariIni = LocalDate.now();
+        
+        
+        System.out.println("========================================");
+        System.out.println(" APLIKASI PENGINGAT MINUM AIR SEDERHANA ");
+        System.out.println("========================================");
+         
+        try {
+             System.out.println("Masukkan nama anda: ");
+             String nama = input.nextLine();
+             
+             System.out.println("Masukkan target minum harian (ml): ");
+             int target = input.nextInt();
+             
+             WaterReminder reminder = new WaterReminder(nama, target);
+             
+             int pilihan = -1;
+             while (pilihan != 0){
+                 System.out.println("\n==== MENU ===");
+                 System.out.println("1. Tambah Minum Hari ini");
+                 System.out.println("2. Lihat Progress Hari ini");
+                 System.out.println("3. Tambah minum pada tanggal lain");
+                 System.out.println("4. Edit minum pada tanggal tertentu");
+                 System.out.println("5. Lihat Riwayat Minum");
+                 System.out.println("0. Keluar");
+                 System.out.println("Pilihan :");
+                 pilihan = input.nextInt();
+             }
+         }
     }
 }
